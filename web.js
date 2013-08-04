a4a2dc835c63ab999407643d5ea8567736bb0894
Ubuntu <ubuntu@ip-172-31-18-149.us-west-2.compute.internal>
2013-08-04 18:05:53 +0000

    Added updated web js to read in html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+
+var fs = require('fs');
+var buff = fs.readFileSync("index.html", "utf-8");
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
